a3036937f3b137d7a3917ab0ea4fce3a76e89d6b
James Scolamieri <james.scolamieri@gmail.com>
2020-10-07 22:00:35 -0700

    deleted comments

--- server/router.js
+++ server/router.js
@@ -75,29 +75,6 @@ router.use('/static',
   })
 )
 
-// router.use('/more-data',
-//   createProxyMiddleware({
-//     target: '127.0.0.1',
-//     changeOrigin: true,
-//     router: function() { return 'http://localhost:1128/' }
-//   })
-// )
-
-
-
-// router.get('/place', (req, res) => {
-
-//   axios.get(`http://localhost:1128/place?room_id=${req.query.roomid}`)
-//     .then(function(response){
-//       res.status(200)
-//       res.json(response.data);
-//     })
-//     .catch(function(error){
-//       console.log(error);
-//       res.send(500);
-//     })
-// })
-
 router.get('/place', (req, res) => {
   const id = req.query.roomid;
   axios.get(`http://localhost:1128/place?roomid=${id}`)
@@ -108,56 +85,6 @@ router.get('/place', (req, res) => {
       console.log(err);
     })
 });
-
-
-// router.get('/api/listing/', (req, res) => {
-//   var queryString = `http://localhost:3005/api/listing/?listing_id=${req.query.listing_id}`
-//   axios.get(queryString)
-//     .then(function (response) {
-//     res.send(response)
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//       res.sendStatus(500);
-//     })
-// })
-
-
-// router.get('/gallery', (req, res) => {
-//   res.bundle = {};
-//   res.data = {};
-//   axios.get('http://localhost:3061/listing/:id/bundle.js')
-//     .then(function(response) {
-//       res.send(response.data);
-//     })
-//     .catch(function(error) {
-//       console.log(error);
-//     })
-// })
-    // .then(
-    //   axios.get('http://localhost:3061/data/55')
-    //     .then(function(results) {
-    //       // console.log(results);
-    //       res.data = results.data;
-    //       res.send(200);
-    //     })
-    //     .catch(function(error) {
-    //       console.log(error);
-    //       res.send(500)
-    //     })
-    // )
-    // .catch();
-
-// request information from API;
-// router.get('/more-data/, () => {
-//   queryString = `http://localhost:3061/data/id=${id}`;
-//   console.log(queryString);
-//   res.send(200)
-//   // axios.get()
-// });
-// // update is liked heart;
-// // router.patch('/data/update', controller.updateLiked);
-
 
 
 module.exports = router;
