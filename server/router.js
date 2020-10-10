26e256494ee86c6475956601949912bad89674ca
James Scolamieri <james.scolamieri@gmail.com>
2020-10-10 11:41:19 -0700

    updated addresses for reservations and more-places

--- server/router.js
+++ server/router.js
@@ -7,23 +7,29 @@ const axios = require('axios');
 // serves index as static
 router.use('/', express.static(path.join(__dirname, '../public')));
 
+const addresses = {
+  gallery: 'http://localhost.3061/',
+  reservations: 'http://ec2-3-135-212-193.us-east-2.compute.amazonaws.com/',
+  morePlaces: 'http://54.183.142.121/'
+};
+
 
 // ----- GALLERY ------
 // had to remove reg ex and hardcode the id to get images to render
 
-router.use('/bundle.js',
-  createProxyMiddleware({
-    target: '127.0.0.1',
-    changeOrigin: true,
-    router: function() { return 'http://localhost:3061/listing/:id' }
-  })
-);
+// router.use('/bundle.js',
+//   createProxyMiddleware({
+//     target: '127.0.0.1',
+//     changeOrigin: true,
+//     router: function() { return 'http://localhost:3061/listing/:id' }
+//   })
+// );
 
 router.use('/listing',
   createProxyMiddleware({
     target: '127.0.0.1',
     changeOrigin: true,
-    router: function() { return 'http://localhost:3061/' }
+    router: function() { return addresses.gallery }
   })
 );
 
@@ -31,26 +37,26 @@ router.use('/data',
   createProxyMiddleware({
     target: '127.0.0.1',
     changeOrigin: true,
-    router: function() { return 'http://localhost:3061/' }
+    router: function() { return addresses.gallery }
   })
 );
 
 
 // ----- reservations ------
 
-router.use('/reservations',
-  createProxyMiddleware({
-    target: '127.0.0.1',
-    changeOrigin: true,
-    router: function() { return 'http://localhost:3005/public/bundle' }
-  })
-);
+// router.use('/reservations',
+//   createProxyMiddleware({
+//     target: '127.0.0.1',
+//     changeOrigin: true,
+//     router: function() { return 'http://http://ec2-3-135-212-193.us-east-2.compute.amazonaws.com/public/bundle' }
+//   })
+// );
 
 router.use('/api/listing/',
   createProxyMiddleware({
     target: '127.0.0.1',
     changeOrigin: true,
-    router: function() { return 'http://localhost:3005' }
+    router: function() { return addresses.reservations }
   })
 )
 
@@ -58,7 +64,7 @@ router.use('/api/reservations/',
   createProxyMiddleware({
     target: '127.0.0.1',
     changeOrigin: true,
-    router: function() { return 'http://localhost:3005' }
+    router: function() { return addresses.reservations }
   })
 )
 
@@ -67,17 +73,17 @@ router.use('/api/reservations/',
 // ----- MORE PLACES TO STAY -----
 // had to change main route for this component to static to get it to work
 
-router.use('/static',
-  createProxyMiddleware({
-    target: '127.0.0.1',
-    changeOrigin: true,
-    router: function() { return 'http://localhost:1128/' }
-  })
-)
+// router.use('/static',
+//   createProxyMiddleware({
+//     target: '127.0.0.1',
+//     changeOrigin: true,
+//     router: function() { return 'http://54.183.142.121/' }
+//   })
+// )
 
 router.get('/place', (req, res) => {
   const id = req.query.roomid;
-  axios.get(`http://localhost:1128/place?roomid=${id}`)
+  axios.get(`${addresses.morePlaces}place?roomid=${id}`)
     .then((response) => {
       res.status(200).send(response.data);
     })
